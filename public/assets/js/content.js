6ba98d0dbf3f5931511b3347a28e9466ec817eeb
AlburIvan <albur.ivan@outlook.com>
2017-09-04 23:33:35 -0400

    Add typed library

---
+++ public/assets/js/content.js
@@ -0,0 +1,12 @@
+var typed = new Typed('.developer-name', {
+    strings: [
+        "Ivan A. Alburquerque."
+    ],
+    typeSpeed: 70,
+    startDelay: 1000,
+    backDelay: 2000,
+    showCursor: true,
+    cursorChar: '|',
+    autoInsertCss: true,
+    loop: false,
+});
