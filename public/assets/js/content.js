891d7f4125b02f81032412a0b8d7f8cc03b00778
AlburIvan <albur.ivan@outlook.com>
2017-09-06 03:25:22 -0400

    [WP] Fixing skills section

--- public/assets/js/content.js
+++ public/assets/js/content.js
@@ -2,13 +2,12 @@
 var typed = new Typed('.developer-name', {
     strings: [
         "Ivan A. Alburquerque",
-        "Android Developer"
+        "Ivan A. Alburquerque</br>Android Developer"
     ],
-    typeSpeed: 35,
+    typeSpeed: 25,
     startDelay: 1000,
     backDelay: 200,
-    showCursor: true,
-    cursorChar: '|',
+    showCursor: false,
     loop: false,
 });
 
@@ -16,3 +15,41 @@ var typed = new Typed('.developer-name', {
 particlesJS.load('particles-js', 'particles.json', function() {
     console.log('callback - particles.js config loaded');
 });
+
+var pieData = [
+    {
+       value: 25,
+       label: 'Android',
+       color: '#811BD6'
+    },
+    {
+       value: 10,
+       label: 'Java',
+       color: '#9CBABA'
+    },
+    {
+       value: 30,
+       label: 'PHP',
+       color: '#D18177'
+    },
+    {
+       value : 35,
+       label: 'HTML',
+       color: '#6AE128'
+    }
+ ];
+
+ var ctx = document.getElementById("skills").getContext('2d');
+ var myChart = new Chart(ctx, {
+     type: 'doughnut',
+     data: data = {
+        datasets: [{
+            data: [80, 20],
+        }],
+    
+        // These labels appear in the legend and in the tooltips when hovering different arcs
+        labels: [
+            'Android',
+        ]
+    }
+ });
