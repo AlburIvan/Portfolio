f8271c1640fde6dfffac9a213aa515ec12d90698
AlburIvan <albur.ivan@outlook.com>
2017-09-08 02:27:10 -0400

    Add skills route api

--- app/routes.js
+++ app/routes.js
@@ -1,7 +1,7 @@
 module.exports = (function () {
     'use-strict';
 
-    const path   = require('path');
+    const path = require('path');
     const config = require("../config/config.json")
     const router = require('express').Router()
 
@@ -9,8 +9,22 @@ module.exports = (function () {
         res.status(200).json(config)
     })
 
-    router.get('/', function(req, res){
+    router.get('/', function (req, res) {
         res.sendFile(path.resolve('./public/assets/views/index.html'));
+    })
+
+    router.get('/config/skills', function (req, res) {
+
+        var fs = require('fs');
+
+        fs.readFile('./public/assets/raw/skills.json', 'utf8', function (err, data) {
+
+            if (err) {
+                res.status(200).json({error: "skills.json not found on server."});
+            }
+         
+            res.send(JSON.stringify(JSON.parse(data), null, 2));
+        });
     })
 
     return router;
