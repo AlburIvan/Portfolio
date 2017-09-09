9b4dd16300fb4f546d0e3fd5d9736dc8fca03e0d
AlburIvan <albur.ivan@outlook.com>
2017-09-09 15:32:40 -0400

    Add Carousel, scroll function

--- public/assets/js/content.js
+++ public/assets/js/content.js
@@ -1,50 +1,77 @@
-// Typed.js Initialitation
-var typed = new Typed('.developer-name', {
-    strings: [
-        "Ivan A. Alburquerque",
-        "Ivan A. Alburquerque</br>Android Developer"
-    ],
-    typeSpeed: 25,
-    startDelay: 1000,
-    backDelay: 200,
-    showCursor: false,
-    loop: false,
-});
+$(function() {
 
-// ParticleJS Initialitation
-particlesJS.load('particles-js', 'particles.json', null);
+     // Typed.js Initialitation
+     var typed = new Typed('.developer-name', {
+        strings: [
+            "Ivan A. Alburquerque",
+        ],
+        typeSpeed: 15,
+        startDelay: 750,
+        showCursor: false,
+        loop: false,
+    });
 
-$.ajax({
-    url: "/config/skills",
-})
-.done(function(data) {
-    
-    let jsonData = JSON.parse(data);
+    // Typed.js Initialitation
+    var typed = new Typed('.developer-title', {
+        strings: [
+            "Android Developer",
+            "Full Stack Engineer",
+            "Kotlin Lover",
+            "Bug Neurosurgeon",
+            "The guy who keeps reading StackOverflow"
+        ],
+        typeSpeed: 25,
+        startDelay: 1550,
+        backDelay: 850,
+        showCursor: true,
+        loop: true,
+    });
 
-    // iterate the result here and populate my skills from the skills.json file
-    // we should add a target property with the element to be loaded
-    jsonData.forEach(function(element) {
-
-        if($(element.target).length == 0)
-            return;
-      
-        var skillBar = new ProgressBar.Circle(element.target, {
-            color: element.color,
-            trailColor: element.trailColor,
-            trailWidth: element.trailWidth,
-            duration: element.duration,
-            easing: element.easing,
-            strokeWidth: element.strokeWidth,
-            from: { color: element.from.color, a:0 },
-            to: { color: element.to.color, a:1 },
-            // Set default step function for all animate calls
-            step: function(state, circle) {
-                circle.path.setAttribute('stroke', state.color);
-            }
-        });
-
-        skillBar.animate(element.animate);
-
-    }, this);
+    // ParticleJS Initialitation
+    particlesJS.load('particles-js', 'particles.json', null);
    
+    $('#ubea-services').on('inview', function(event, isInView) {
+        if (isInView) {
+            setTimeout(function() {
+                $.ajax({
+                    url: "/config/skills",
+                })
+                .done(function(data) {
+                    
+                    let jsonData = JSON.parse(data);
+                
+                    // Iterate the result here and populate my skills from the skills.json file
+                    // we should add a target property with the element to be loaded
+                    jsonData.forEach(function(element) {
+                
+                        if($(element.target).length == 0)
+                            return;
+                      
+                        var skillBar = new ProgressBar.Circle(element.target, {
+                            color: element.color,
+                            trailColor: element.trailColor,
+                            trailWidth: element.trailWidth,
+                            duration: element.duration,
+                            easing: element.easing,
+                            strokeWidth: element.strokeWidth,
+                            from: { color: element.from.color, a:0 },
+                            to: { color: element.to.color, a:1 },
+                            // Set default step function for all animate calls
+                            step: function(state, circle) {
+                                circle.path.setAttribute('stroke', state.color);
+                            }
+                        });
+                
+                        skillBar.animate(element.animate);
+                
+                    }, this);
+                   
+                });
+            }, 450);
+        } else {
+            $('#ubea-services').off('inview');
+        }
+    });
+
+
 });
